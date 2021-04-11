0882fdef03f484c8ab3e24f2bbdf9cfd77e2f47f
enolaholmesbot <82078010+enolaholmesbot@users.noreply.github.com>
2021-04-11 13:58:16 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '6288235435804', 'Admin DrawlNag', m)
+  // this.sendContact(m.chat, '6282292119677', 'Andi', m)
+  this.sendContact(m.chat, '6282292119677', 'Andi', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
